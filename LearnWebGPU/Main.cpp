d851b419e64232e39df2ef3371c0d47b8519114a
agandia <aitor_gv@hotmail.com>
2026-02-23 09:33:29 +0100

    Add project files.

---
+++ LearnWebGPU/Main.cpp
@@ -0,0 +1,114 @@
+﻿/**
+ * This file is part of the "Learn WebGPU for C++" book.
+ *   https://github.com/eliemichel/LearnWebGPU
+ *
+ * MIT License
+ * Copyright (c) 2022-2024 Elie Michel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+* Although based on the licensed implementation above, this file contains modifications
+* that are not part of the original work. These modifications are a result of a learning
+* exercise on my end.
+*/
+#include "webgpu-utils.h"
+#include <webgpu/webgpu.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif // __EMSCRIPTEN__
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+int main(int, char**)
+{
+	// We create the descriptor
+  WGPUInstanceDescriptor desc = {};
+  desc.nextInChain = nullptr;
+
+  // We create the instance using this descriptor
+#ifdef WEBGPU_BACKEND_EMSCRIPTEN
+  WGPUInstance instance = wgpuCreateInstance(nullptr);
+  std::cout << "EMSCRIPTEN BACKENDED INSTANCE" << std::endl;
+#else
+  WGPUInstance instance = wgpuCreateInstance(&desc);
+  std::cout << "WGPU/DAWN BACKENDED INSTANCE" << std::endl;
+#endif // WEBGPU_BACKEND_EMSCRIPTEN
+
+  // Check if the instance was created successfully
+  if (!instance) {
+    std::cerr << "Failed to create WebGPU instance. Could not initialize WebGPU" << std::endl;
+    return 1;
+  }
+
+  // Display the object (WGPUInstance is a simple pointer, it may be
+  // copied around without worrying about its size).
+  std::cout << "WGPU instance: " << instance << std::endl;
+
+  std::cout << "Requesting adapter..." << std::endl;
+  WGPURequestAdapterOptions adapterOpts = {};
+  adapterOpts.nextInChain = nullptr;
+  WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+  std::cout << "Got adapter: " << adapter << std::endl;
+
+  // Display some information about the adapter
+  inspectAdapter(adapter);
+
+  // It is good practice to release the instance as soon as we have the adapter.
+  wgpuInstanceRelease(instance);
+
+  std::cout << "Requesting device..." << std::endl;
+  WGPUDeviceDescriptor deviceDesc = {};
+  deviceDesc.nextInChain = nullptr;
+  deviceDesc.label = "My Device"; // anything works here, that's your call
+  deviceDesc.requiredFeatureCount = 0; // we do not require any specific feature
+  deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+  deviceDesc.defaultQueue.nextInChain = nullptr;
+  deviceDesc.defaultQueue.label = "The default queue";
+  // A function that is invoked whenever the device stops being available.
+  deviceDesc.deviceLostCallback = [](WGPUDeviceLostReason reason, char const* message, void* /* pUserData */) {
+    std::cout << "Device lost: reason " << reason;
+    if (message) std::cout << " (" << message << ")";
+    std::cout << std::endl;
+    };
+  WGPUDevice device = requestDeviceSync(adapter, &deviceDesc);
+  std::cout << "Got device: " << device << std::endl;
+
+  // A function that is invoked whenever there is an error in the use of the device
+  auto onDeviceError = [](WGPUErrorType type, char const* message, void* /* pUserData */) {
+    std::cout << "Uncaptured device error: type " << type;
+    if (message) std::cout << " (" << message << ")";
+    std::cout << std::endl;
+    };
+  wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
+
+  // And equally good practice to release the adapter as soon as we have the device.
+  wgpuAdapterRelease(adapter);
+
+  // Display information about the device
+  inspectDevice(device);
+
+  wgpuDeviceRelease(device);
+
+	return 0;
+}
